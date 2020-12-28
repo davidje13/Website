9047154e0a98d66ae0a29d51ef10b27e47048c8d
David Evans <davidje13@users.noreply.github.com>
2020-12-28 18:40:23 +0000

    Run refacto directly rather than via node command to get shebang-configured runtime flags

--- refacto/runner.sh
+++ refacto/runner.sh
@@ -5,7 +5,6 @@ export TRUST_PROXY=true;
 
 mkdir -p "$BASEDIR/logs/log$PORT";
 
-node "$BASEDIR/build/index.js" 2>&1 \
-  > >(multilog n50 s1048576 "$BASEDIR/logs/log$PORT") &
+"$BASEDIR/build/index.js" 2>&1 > >(multilog n50 s1048576 "$BASEDIR/logs/log$PORT") &
 
 echo "$!" > "$BASEDIR/logs/pid$PORT";
