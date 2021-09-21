288fe4ec364268dcb7330e5421d1d0a62a4f537a
David Evans <davidje13@users.noreply.github.com>
2021-09-21 20:00:37 +0100

    Ensure www installer does not remove existing site until new site is ready

--- www/installer.sh
+++ www/installer.sh
@@ -2,22 +2,25 @@ set -ex
 
 BASEDIR="$(dirname "$0")";
 
-# Remove existing site if found
-sudo rm -r /var/www/https || true;
+INSTALL_DIR="/var/www/https";
+INSTALL_TEMP_DIR="/var/www/https2";
+
+# Clear temp installation folder if found
+sudo rm -r "$INSTALL_TEMP_DIR" || true;
 
 # Install
-sudo mkdir -p /var/www/https/errors;
+sudo mkdir -p "$INSTALL_TEMP_DIR/errors";
 sudo cp \
   "$BASEDIR/style.css" \
   "$BASEDIR/favicon.png" \
   "$BASEDIR/robots.txt" \
   "$BASEDIR/ads.txt" \
-  /var/www/https/;
+  "$INSTALL_TEMP_DIR/";
 
 sed \
   "s/((DOMAIN))/$DOMAIN/g" \
   "$BASEDIR/index.htm" | \
-  sudo tee /var/www/https/index.htm > /dev/null;
+  sudo tee "$INSTALL_TEMP_DIR/index.htm" > /dev/null;
 
 make_error_page() {
   CODE="$1";
@@ -27,7 +30,7 @@ make_error_page() {
     -e "s/((DOMAIN))/$DOMAIN/g" \
     -e "s/((ERROR))/$ERROR/g" \
     "$BASEDIR/error.htm" | \
-    sudo tee "/var/www/https/errors/$CODE.htm" > /dev/null;
+    sudo tee "$INSTALL_TEMP_DIR/errors/$CODE.htm" > /dev/null;
 }
 
 while IFS='' read -r LINE; do
@@ -36,7 +39,11 @@ while IFS='' read -r LINE; do
   fi;
 done < "$BASEDIR/http_statuses";
 
-sudo chown -R root:www-data /var/www/https;
+sudo chown -R root:www-data "$INSTALL_TEMP_DIR";
+
+# Remove existing site if found and move new site in place
+sudo rm -r "$INSTALL_DIR" || true;
+sudo mv "$INSTALL_TEMP_DIR" "$INSTALL_DIR";
 
 # Add NGINX config
 
