9a876373ceb2dbd6d035fce8263550b3a0a3d740
David Evans <davidje13@users.noreply.github.com>
2023-03-19 10:03:30 +0000

    Update sequence diagram entrypoint

--- sequence/runner.sh
+++ sequence/runner.sh
@@ -5,7 +5,7 @@ PORT="$1";
 mkdir -p "$BASEDIR/logs/log$PORT";
 
 FONTDIR="$BASEDIR/fonts" \
-"$BASEDIR/bin/server.js" "$PORT" 2>&1 \
+"$BASEDIR/dev-bin/server.mjs" "$PORT" 2>&1 \
   > >(multilog t n50 s1048576 "$BASEDIR/logs/log$PORT") &
 
 echo "$!" > "$BASEDIR/logs/pid$PORT";
