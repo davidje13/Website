72ddd30473b94241aa5fe1e76eb7e705a80b8980
David Evans <davidje13@users.noreply.github.com>
2023-08-27 00:21:44 +0100

    Fix capturing stderr in logs

--- sequence/runner.sh
+++ sequence/runner.sh
@@ -5,7 +5,7 @@ PORT="$1";
 mkdir -p "$BASEDIR/logs/log$PORT";
 
 FONTDIR="$BASEDIR/fonts" \
-"$BASEDIR/dev-bin/server.mjs" "$PORT" 2>&1 \
-  > >(multilog t n50 s1048576 "$BASEDIR/logs/log$PORT") &
+"$BASEDIR/dev-bin/server.mjs" "$PORT" \
+  > >(multilog t n50 s1048576 "$BASEDIR/logs/log$PORT") 2>&1 &
 
 echo "$!" > "$BASEDIR/logs/pid$PORT";
