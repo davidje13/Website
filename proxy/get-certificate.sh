38420c043c83d869d05d02a79bd7e6acc90134ec
David Evans <davidje13@users.noreply.github.com>
2023-10-08 14:40:28 +0100

    Show IP match when requesting certificate

--- proxy/get-certificate.sh
+++ proxy/get-certificate.sh
@@ -9,14 +9,20 @@ if [[ ! -f /etc/letsencrypt/live/all/fullchain.pem ]]; then
     # fetch own IPv6 address
     MYIP6="$(dig -6 +short myip.opendns.com AAAA @resolver1.opendns.com)";
     DNS_AAAA="$(dig +short "$(head -n1 "/var/www/domains.txt")" AAAA @8.8.8.8)";
-    if [[ -n "$DNS_AAAA" && "$DNS_AAAA" == "$MYIP6" ]]; then break; fi;
+    if [[ -n "$DNS_AAAA" && "$DNS_AAAA" == "$MYIP6" ]]; then
+      echo "IPv6 address $MYIP6 matches domain - requesting certificate";
+      break;
+    fi;
 
     # only check IPv4 if there is no IPv6 record, since certbot will always prefer IPv6 if present
     if [[ -z "$DNS_AAAA" ]]; then
       # can also use e.g. $(curl -s4 https://checkip.amazonaws.com)
       MYIP4="$(dig -4 +short myip.opendns.com A @resolver1.opendns.com)";
       DNS_A="$(dig +short "$(head -n1 "/var/www/domains.txt")" A @8.8.8.8)";
-      if [[ -n "$DNS_A" && "$DNS_A" == "$MYIP4" ]]; then break; fi;
+      if [[ -n "$DNS_A" && "$DNS_A" == "$MYIP4" ]]; then
+        echo "IPv4 address $MYIP4 matches domain (IPv6 not configured) - requesting certificate";
+        break;
+      fi;
     fi;
 
     if [[ " $* " == *" --immediate "* ]]; then
