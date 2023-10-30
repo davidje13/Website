6cb347e6e9077b758f728a6fd9d6174aaf187062
David Evans <davidje13@users.noreply.github.com>
2023-10-30 19:13:19 +0000

    Ensure ws does not attempt to load packages which do not exist

--- refacto/runner.sh
+++ refacto/runner.sh
@@ -3,6 +3,10 @@ BASEDIR="$(dirname "$0")";
 export PORT="$1";
 export TRUST_PROXY=true;
 
+# configure ws optional dependencies
+export WS_NO_BUFFER_UTIL=true; # do not look for bufferutil dependency which is not installed
+export WS_NO_UTF_8_VALIDATE=true; # not required in Node >= 18.14.0
+
 mkdir -p "$BASEDIR/logs/log$PORT";
 
 "$BASEDIR/build/index.js" \
